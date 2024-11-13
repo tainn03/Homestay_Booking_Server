4732c4e3925923509b34cfe7ca21b17770e892bb
NGUYEN NHAT TAI <tainn.dv@gmail.com>
2024-11-13 10:05:17 +0700

    fix: remove cache

--- src/main/java/com/homestay/config/ProjectSecurityConfig.java
+++ src/main/java/com/homestay/config/ProjectSecurityConfig.java
@@ -7,7 +7,6 @@ import com.homestay.filter.JwtAuthenticationFilter;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -34,7 +33,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 @EnableScheduling
-@EnableCaching
+//@EnableCaching
 public class ProjectSecurityConfig {
     AuthenticationProvider authenticationProvider;
     JwtAuthenticationFilter jwtAuthenticationFilter;
